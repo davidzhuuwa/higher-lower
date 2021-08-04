955e856dd897ccd9184207cd1beb2c5ed1ba1ec7
David Zhu <63690620+davidzhuuwa@users.noreply.github.com>
2021-08-04 23:10:17 +0800

    Cleaned up function docstrings and added some comments

--- main.py
+++ main.py
@@ -10,14 +10,19 @@ game_state = True
 
 # Comparison lines A and B, which uses information from data dictionary with the person's name, occupation and location
 def select_person():
+    """ Selects an instagram account from 'data' and extracts the name, 
+    follower count, description and country, as well as the full dictionary 
+    entry """ 
     choice = rd.choice(data)
     name = choice['name']
     follower_count = choice['follower_count']
     description = choice['description']
     country = choice['country']
     return name,follower_count,description,country,choice 
-
+    
 def compare(followerA,followerB, user_choice):
+    """ Determines if Account A has more followers than Account B, and asserts 
+    whether the user has guessed correctly. """
     Amore = False
     Bmore = False 
     Tie = False 
@@ -39,32 +44,41 @@ def compare(followerA,followerB, user_choice):
     return correct_choice,Tie 
 
 def reset_graphics():
+    """ Clears the screen and prints out the higher lower graphic. """
     clear()
     print(logo)
 
-nameA,followerA,descriptionA,countryA,dataA = select_person()
 nameB,followerB,descriptionB,countryB,dataB = select_person()
 while game_state:
+    # Setting Account A as Account B 
+    nameA = nameB
+    followerA = followerB
+    descriptionA = descriptionB
+    countryA = countryB 
+    dataA = dataB
+    nameB,followerB,descriptionB,countryB,dataB = select_person()
+    
+    # Making sure the accounts are not the same 
+    if dataA == dataB:
+        accounts_equal = True 
+        while accounts_equal:
+            nameB,followerB,descriptionB,countryB,dataB = select_person()
+            if dataA != dataB:
+                accounts_equal = False
+ 
+    
+    # Printing output for game start 
     print(f"Compare A: {nameA}, a {descriptionA}, from {countryA}.")
     print(vs)
     print(f"Against B: {nameB}, a {descriptionB}, from {countryB}.")
     choice = input('Who has more followers? Type "A" or "B": ').lower()
+    
     correct_choice,Tie = compare(followerA=followerA,followerB=followerB,user_choice=choice)
     if correct_choice:
         score+=1
         reset_graphics()
         print(f"You're right! Current score: {score}.")
-        nameA = nameB
-        followerA = followerB
-        descriptionA = descriptionB
-        countryA = countryB 
-        nameB,followerB,descriptionB,countryB,dataB = select_person()
-        if dataA == dataB: 
-            account_equal = True
-            while account_equal:
-                nameB,followerB,descriptionB,countryB,dataB = select_person()
-                if dataA != dataB:
-                    account_equal = False
+
     elif Tie:
         reset_graphics()
         print(f"The number of followers are equal! The game is not over \n but your score remains the same.")
